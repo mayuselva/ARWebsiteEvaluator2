832edb8e9e14067c45a2712cbc7c2a7cf65342b6
ShaneFuzz <ashen.kavindra@gmail.com>
2016-09-10 13:52:41 +0530

    Initial commit

---
+++ app/src/main/java/shanefuzz/arwebsiteevaluator/UserSignIn.java
@@ -0,0 +1,296 @@
+package shanefuzz.arwebsiteevaluator;
+
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.graphics.Paint;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Gravity;
+import android.view.KeyEvent;
+import android.view.View;
+import android.widget.AutoCompleteTextView;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.Random;
+
+public class UserSignIn extends AppCompatActivity {
+
+
+
+        private static final String TAG = "SignInActivity";
+        public static UserProfile id;
+
+        // UI references.
+        private AutoCompleteTextView mEmailView;
+        private EditText mPasswordView;
+        private TextView mRecovery;
+
+        private ConnectivityManager mConMan = null;
+        private NetworkInfo mInfo = null;
+
+        private DatabaseHandler_user mDatabaseHandler;
+
+
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            //Utils.setThemeToActivity(this);
+            setContentView(R.layout.activity_user_sign_in);
+
+            Log.i(TAG, "onCreate");
+
+            // Set up login details
+            mEmailView = (AutoCompleteTextView) findViewById(R.id.text_signin_email);
+            mPasswordView = (EditText) findViewById(R.id.text_signin_password);
+
+            mRecovery = (TextView) findViewById(R.id.text_recovery);
+            mRecovery.setPaintFlags(mRecovery.getPaintFlags() | Paint.UNDERLINE_TEXT_FLAG);
+
+            // Connect to the database
+            mDatabaseHandler = new DatabaseHandler_user(this, null, null, 1);
+
+            // Login by pressing the enter key(Sign in-key)
+            mPasswordView.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+                @Override
+                public boolean onEditorAction(TextView textView, int id, KeyEvent keyEvent) {
+
+                    return signInTask();
+
+                }
+
+
+            });
+
+
+            // Login by pressing sign in button
+            Button mEmailSignInButton = (Button) findViewById(R.id.email_sign_in_button);
+            mEmailSignInButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+
+                    signInTask();
+
+                }
+            });
+
+            // Recover user password
+            mRecovery.setOnClickListener(new View.OnClickListener() {
+
+                public void onClick(View v) {
+
+                    sendRecoveryPasswordEmail();
+
+                }
+            });
+
+
+        }
+
+
+
+
+        /**
+         * Attempts to sign in.
+         * If there are form errors (invalid fields, missing fields, etc.), the
+         * errors are presented and no actual login attempt is made.
+         */
+    private boolean signInTask() {
+
+        String email = mEmailView.getText().toString();
+        String password = mPasswordView.getText().toString();
+
+        if (isEmpty(email)) {
+            mEmailView.setError("This field is required");
+        }
+
+        if (isEmpty(password)) {
+
+            mPasswordView.setError("This field is required");
+
+        } else if (!isEmpty(password) && !isEmpty(email) && !mDatabaseHandler.authenticateUserLogin(email, password)) {
+
+            Toast toast = Toast.makeText(getApplicationContext(), "Invalid email or password. Please try again.", Toast.LENGTH_LONG);
+            toast.setGravity(Gravity.CENTER, 0, 0);
+            toast.show();
+
+        } else {
+            setUserObject();
+        }
+
+        return true;
+
+    }
+
+
+    /**
+     * Send an email with 8 characters new random password to user's email address.
+     */
+    private void sendRecoveryPasswordEmail() {
+
+        mPasswordView.setError(null);
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(UserSignIn.this);
+        builder.setMessage("Are you sure you want to reset your password?");
+        builder.setIcon(android.R.drawable.ic_dialog_alert);
+        builder.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+
+                mConMan = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+                mInfo = mConMan.getActiveNetworkInfo();
+
+                if (mInfo == null) {
+                    Toast.makeText(getApplicationContext(), "Unable to send confirmation email. Please check your Internet connection.", Toast.LENGTH_LONG).show();
+                } else {
+                    sendRecoveryEmail();
+                }
+
+            }
+        });
+
+        builder.setNegativeButton("No", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.cancel();
+            }
+        });
+
+        AlertDialog alert = builder.create();
+        alert.show();
+
+    }
+
+
+    /**
+     * Go to SignUpActivity page
+     */
+    public void goToSignUpPage(View v) {
+
+        Intent intent = new Intent(this, UserSignUp.class);
+        startActivity(intent);
+        finish();
+
+    }
+
+
+    /**
+     * Return user object from the database
+     */
+    public void setUserObject() {
+
+        String email = mEmailView.getText().toString();
+        String password = mPasswordView.getText().toString();
+
+        findViewById(R.id.email_sign_in_button).setVisibility(View.GONE);
+        findViewById(R.id.login_progress).setVisibility(View.VISIBLE);
+
+        UserProfile user = mDatabaseHandler.createUserObject(email, password);
+
+        id=user;
+
+        //Intent intent = new Intent(this, Dump_expenses_list.class);
+        //intent.putExtra("user", user);
+        //startActivity(intent);
+        //SignInActivity.this.finish();
+
+        Intent intent = new Intent("android.media.action.IMAGE_CAPTURE");
+        startActivity(intent);
+
+
+
+    }
+
+
+    /**
+     * Empty field validation
+     */
+    private boolean isEmpty(String field) {
+
+        return field.isEmpty();
+    }
+
+    /**
+     * Generate 6 character random password
+     */
+    private static final Random random = new Random();
+    private static final String CHARS = "abcdefghijkmnopqrstuvwxyzABCDEFGHJKLMNOPQRSTUVWXYZ234567890!@#$";
+
+    public static String getToken() {
+        StringBuilder token = new StringBuilder(8);
+        for (int i = 0; i < 8; i++) {
+            token.append(CHARS.charAt(random.nextInt(CHARS.length())));
+        }
+        return token.toString();
+    }
+
+
+    /**
+     * press back key within 3 seconds for the second time to close the app
+     */
+    private Boolean exit = false;
+
+    @Override
+    public void onBackPressed() {
+        if (exit) {
+            finish(); // finish activity
+        } else {
+            Toast.makeText(this, "Press Back again to Exit.",
+                    Toast.LENGTH_SHORT).show();
+            exit = true;
+            new Handler().postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    exit = false;
+                }
+            }, 3 * 1000);
+
+        }
+
+    }
+
+
+    /**
+     * Send password recovery email to the user
+     */
+    private void sendRecoveryEmail() {
+
+        String email = mEmailView.getText().toString();
+
+
+        // Getting content for email
+        if (isEmpty(email)) {
+            mEmailView.setError("This field is required.");
+        } else if (!mDatabaseHandler.isEmailStore(email)) {
+            mEmailView.setError("Sorry, Couldn't find an account with this email address. Please try again.");
+        } else {
+
+            String token = getToken();
+            mDatabaseHandler.resetUserPassword(email, token);
+
+            String subject = "AR Website Evaluator: Forgot your password?";
+            String message = "Here is your new password for AR Website Evaluator:" + token + "\n Log in to the application using this password, and remember to change this given password once you log in.";
+
+
+            // Creating SendMail object
+            SendMail sm = new SendMail(this, email, subject, message);
+
+            // Executing SendMail to send email
+            sm.execute();
+
+            Toast toast = Toast.makeText(getApplicationContext(), "You will be received an email shortly with a new password.", Toast.LENGTH_LONG);
+            toast.setGravity(Gravity.BOTTOM, 0, 0);
+            toast.show();
+        }
+    }
+
+}
---
+++ app/src/main/java/shanefuzz/arwebsiteevaluator/UserSignUp.java
@@ -0,0 +1,324 @@
+package shanefuzz.arwebsiteevaluator;
+
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Gravity;
+import android.view.KeyEvent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class UserSignUp extends AppCompatActivity {
+
+    private static final String TAG = "SignUpActivity";
+
+    // UI references.
+    private EditText mEmailView;
+    private EditText mNameView;
+    private EditText mPhoneView;
+    private EditText mPasswordView;
+
+
+    private ConnectivityManager mConMan = null;
+    private NetworkInfo mInfo = null;
+
+    private DatabaseHandler_user mDatabaseHandler;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //Utils.setThemeToActivity(this);
+        setContentView(R.layout.activity_user_sign_up);
+
+        Log.i(TAG, "onCreate");
+
+
+        // Set up the SignUpActivity details.
+        //mNameView = (EditText) findViewById(R.id.text_signup_name);
+        mEmailView = (EditText) findViewById(R.id.text_signup_email);
+        mPasswordView = (EditText) findViewById(R.id.text_signup_password);
+        //mPhoneView = (EditText) findViewById(R.id.text_signup_phone);
+
+        // Connect to the database
+        mDatabaseHandler = new DatabaseHandler_user(this, null, null, 1);
+
+
+        // SignUp by pressing the enter key(sign up-key)
+        mPhoneView.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView textView, int id, KeyEvent keyEvent) {
+
+                return signUpTask();
+
+            }
+
+
+        });
+
+
+        // Sign up by pressing the SIGN UP button
+        Button mEmailSignUpButton = (Button) findViewById(R.id.email_sign_up_button);
+        mEmailSignUpButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                signUpTask();
+
+            }
+        });
+
+
+    }
+
+
+    /**
+     * Attempts to sign up.
+     * If there are form errors (invalid fields, missing fields, etc.), the
+     * errors are presented and no actual sign up attempt is made.
+     */
+    private boolean signUpTask() {
+
+
+        emailValidation();
+        nameValidation();
+        passwordValidation();
+
+        if (!isEmpty(mPhoneView) && !isValidNo(mPhoneView)) {
+            mPhoneView.setError("Please enter only numbers.");
+        } else if (!isEmpty(mNameView) && !isEmpty(mEmailView) && !isEmpty(mPasswordView) && isValidPassword(mPasswordView) && isValidEmail(mEmailView.getText().toString()) && !mDatabaseHandler.isEmailStore(mEmailView.getText().toString())) {
+
+            mConMan = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+            mInfo = mConMan.getActiveNetworkInfo();
+
+            if (mInfo == null) {
+                Toast.makeText(getApplicationContext(), "Unable to send confirmation email. Please check your Internet connection.", Toast.LENGTH_LONG).show();
+            } else {
+                sendConfirmationEmail();
+                addUser();
+            }
+
+        }
+
+        return true;
+
+    }
+
+    /**
+     * check email is empty or not
+     * check email is valid one
+     *
+     */
+    public boolean emailValidation() {
+        if (isEmpty(mEmailView)) {
+            mEmailView.setError("This field is required.");
+        } else if (mDatabaseHandler.isEmailStore(mEmailView.getText().toString())) {
+            mEmailView.setError("This email address is already taken. Please choose another one.");
+        } else if (!isValidEmail(mEmailView.getText().toString())) {
+            mEmailView.setError("Please enter a valid email address.");
+        }
+
+        return true;
+    }
+
+
+    /**
+     * check name is empty or not
+     * check name is valid one
+     *
+     */
+    public boolean nameValidation() {
+        if (isEmpty(mNameView)) {
+            mNameView.setError("This field is required.");
+        } else {
+            mNameView.setError(null);
+        }
+
+        return true;
+    }
+
+
+    /**
+     * check password is empty or not
+     * check password is valid one
+     *
+     */
+    public boolean passwordValidation() {
+        if (isEmpty(mPasswordView)) {
+            mPasswordView.setError("This field is required.");
+        } else if (!isValidPassword(mPasswordView)) {
+            mPasswordView.setError("Please enter a valid password.");
+        }
+
+        return true;
+    }
+
+
+    /**
+     * Go to SignInActivity page
+     */
+    public void goToSignInActivity(View v) {
+
+        Intent intent = new Intent(this, UserSignIn.class);
+        startActivity(intent);
+        finish();
+    }
+
+    /**
+     * Add user to database
+     */
+    public void addUser() {
+
+
+        UserProfile user = new UserProfile(mNameView.getText().toString().trim(), mEmailView.getText().toString().trim(), mPasswordView.getText().toString().trim(), mPhoneView.getText().toString().trim());
+        mDatabaseHandler.addUser(user);
+
+
+        mNameView.setText("");
+        mEmailView.setText("");
+        mPasswordView.setText("");
+        mPhoneView.setText("");
+
+        Toast toast = Toast.makeText(getApplicationContext(), "Your account has been successfully created. Please check your email for the confirmation.", Toast.LENGTH_LONG);
+        toast.setGravity(Gravity.CENTER, 0, 0);
+        toast.show();
+
+
+        Log.e(TAG, "Details Added");
+
+        Intent intent = new Intent(this, UserSignIn.class);
+        startActivity(intent);
+        finish();
+
+    }
+
+
+    /**
+     * Send confirmation email to the user
+     */
+    private void sendConfirmationEmail() {
+
+        //Getting content for email
+        String email = mEmailView.getText().toString();
+        String name = mNameView.getText().toString();
+        String subject = "AR Website Evaluator: Account created successfully.";
+        String message = "Congratulation " + name + ", Your account has been created. \n You can log in to the account by using the both email address and password you provided.";
+
+        //Creating SendMail object
+        SendMail sm = new SendMail(this, email, subject, message);
+
+        //Executing SendMail to send email
+        sm.execute();
+    }
+
+
+    /**
+     * Empty field validation
+     */
+    private boolean isEmpty(EditText field) {
+
+        if (field.getText().toString().isEmpty()) {
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * Email address validation
+     */
+    private boolean isValidEmail(String email) {
+        String EMAIL_PATTERN = "^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@"
+                + "[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$";
+
+        Pattern pattern = Pattern.compile(EMAIL_PATTERN);
+        Matcher matcher = pattern.matcher(email);
+        return matcher.matches();
+    }
+
+    /**
+     * Password validation.
+     * At least 8 characters.
+     */
+    private boolean isValidPassword(EditText filed) {
+        if (filed.getText().toString() != null && filed.getText().toString().length() > 7) {
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * Phone number validation
+     */
+    private boolean isValidNo(EditText filed) {
+
+        String NO_PATTERN = "[0-9]+";
+
+        Pattern pattern = Pattern.compile(NO_PATTERN);
+        Matcher matcher = pattern.matcher(filed.getText().toString());
+        return matcher.matches();
+
+
+    }
+
+
+    /**
+     * Discard changes and go back
+     */
+    @Override
+    public void onBackPressed() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(UserSignUp.this);
+        if (!isEmpty(mEmailView) || !isEmpty(mPasswordView) || !isEmpty(mPhoneView) || !isEmpty(mNameView)) {
+
+            builder.setMessage("Discard the changes?");
+            builder.setIcon(android.R.drawable.ic_dialog_alert);
+            builder.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+
+                    goBackToSignInActivity();
+                    UserSignUp.this.finish();
+
+                }
+            });
+
+            builder.setNegativeButton("No", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    dialog.cancel();
+                }
+            });
+            AlertDialog alert = builder.create();
+            alert.show();
+            return;
+
+        } else {
+            goBackToSignInActivity();
+        }
+
+
+    }
+
+
+    /**
+     * Go back to sign in
+     */
+    public void goBackToSignInActivity() {
+
+        Intent intent = new Intent(this, UserSignIn.class);
+        startActivity(intent);
+        finish();
+
+    }
+}
